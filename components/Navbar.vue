b5b45b8b545a42280803cfa5ec4945d9df2d8154
CephalonScientia <scientia4wiki@gmail.com>
2022-03-28 19:51:02 -0700

    Using flex containers to format elements

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -10,18 +10,26 @@ export default {
       <input id="search-input" name="search" type="text" size="75" placeholder="Enter a page name (e.g. 'Damage' without quotation marks)">
       <button id="search-button" type="button">Submit ✔️</button>
     </form>
-    <button id="edit-button" type="button">
-      <a id="edit-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">Edit on the wiki</a>
-    </button>
-    <button id="history-button" type="button">
-      <a id="history-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">Page history on wiki</a>
-    </button>
-    <button id="wiki-button" type="button">
-      <a id="wiki-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">See page on the wiki</a>
-    </button>
-    <div class="text-right p-10">
-      ⏲️ Page fetch time: <span id="elapsed-time">N/A</span><br />
-      <!--💾 Content size: <span id="article-size">N/A</span>-->
+    <div class="flex">
+      <div>
+        <button id="edit-button" type="button">
+          <a id="edit-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">Edit on the wiki</a>
+        </button>
+      </div>
+      <div>
+      <button id="history-button" type="button">
+        <a id="history-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">Page history on wiki</a>
+      </button>
+      </div>
+      <div>
+      <button id="wiki-button" type="button">
+        <a id="wiki-button-link" target="_blank" rel="noopener noreferrer" title="Open link in new tab">See page on the wiki</a>
+      </button>
+      </div>
+      <div class="flex-grow text-right">
+        ⏲️ Page fetch time (ms): <span id="elapsed-time">N/A</span><br />
+        💾 Content size (Mb): <span id="article-size">N/A</span>
+      </div>
     </div>
   </div>
 </template>
